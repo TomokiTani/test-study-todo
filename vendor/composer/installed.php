dead5028cf36491d57f34cc2073b093ef930894b
TomomkiTani <hybrid5207hp@outlook.jp>
2022-02-14 22:22:15 +0900

    third commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '0d14329ced9182f2899da34a9e3be083607ddf7b',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '0d14329ced9182f2899da34a9e3be083607ddf7b',
+            'dev_requirement' => false,
+        ),
+    ),
+);
